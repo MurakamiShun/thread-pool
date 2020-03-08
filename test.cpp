3067b48067937d8e535355b649b6170611557cca
MurakamiShun <enshuuritsu.about3@gmail.com>
2020-03-09 01:40:15 +0900

    update

--- test.cpp
+++ test.cpp
@@ -6,50 +6,40 @@
 int main() {
 	using namespace std;
 
-	constexpr int core = 2;
+	constexpr int core = 8;
 
-	thread_pool pool[core];
-	align_array<int> count(core, 64);
-	for (auto& i : count) {
-		i = 0;
-	}
-	
 	// thread group
 	thread_group group(core);
 	auto t = chrono::system_clock::now();
-	group.run();
+	aligned_array<int> sum(core);
 	for (int i = 0; i < 100; i++) {
 		for (int th = 0; th < core; th++) {
-			int& ccc = count[th];
-			group.post([&] {
+			group.post(th, [&sum, th] {
+				int tmp = 0;
 				for (int j = 0; j < 1000000; j++)
-					ccc += 1;
+					sum[th] += 1;
+				sum[th] += tmp;
 			});
 		}
 	}
-	group.join();
+	group.wait();
 	auto multi = (chrono::system_clock::now() - t).count();
 	cout << core << " thread group:\t" << multi << " ns" << endl;
 
 	thread_pool p;
 	t = chrono::system_clock::now();
-	int c = 0;
+	int tmp = 0;
 	for (int i = 0; i < 100; i++) {
 		p.post([&] {
 			for (int j = 0; j < 1000000 * core; j++)
-				c += 1;
+				tmp += 1;
 		});
-		p.run();
 	}
-	p.join();
+	p.wait();
 	auto single = (chrono::system_clock::now() - t).count();
 	cout << "single thread:\t" << single << " ns" << endl;
 
 	cout << "ratio:\t" << static_cast<double>(single) / static_cast<double>(multi) << std::endl;
 	
-
-#ifdef _WIN32
-	system("pause");
-#endif
 	return 0;
 }
--- thread_pool.hpp
+++ thread_pool.hpp
@@ -2,7 +2,7 @@
 #include <functional>
 #include <thread>
 #include <condition_variable>
-#include <list>
+#include <queue>
 #include <new>
 #include <atomic>
 
@@ -20,167 +20,101 @@ private:
 	std::thread thread;
 	std::condition_variable cv;
 	std::mutex mtx;
-	std::list<std::function<void()>> func_list;
-	std::atomic_bool joined = false;
-	std::atomic_bool is_running = false;
+	std::queue<std::function<void()>> tasks;
+	std::atomic_bool stop = false;
 public:
 	thread_pool() {
 		thread = std::thread([this] {
-			while (!joined || func_list.size()) {
+			while (true) {
 				std::function<void()> func;
 				{
 					std::unique_lock<std::mutex> lock(mtx);
-					if (func_list.size() == 0) {
-						cv.wait(lock, [&] {return func_list.size() && is_running; });
-						continue;
+					if (!stop && tasks.empty()) {
+						cv.notify_all();
+						cv.wait(lock);
 					}
-					func = std::move(func_list.front());
-					func_list.pop_front();
+					if (stop) return;
+					func = std::move(tasks.front());
+					tasks.pop();
 				}
 				func();
-				cv.notify_all();
 			}
 		});
 	}
 	~thread_pool() {
-		join();
+		stop = true;
+		cv.notify_all();
+		thread.join();
 	}
 	thread_pool(const thread_pool&) = delete;
 	void post(std::function<void()> func) {
 		std::lock_guard<std::mutex> lock(mtx);
-		func_list.push_back(std::move(func));
+		tasks.push(std::move(func));
 		cv.notify_all();
-	}
-	void run() {
-		is_running = true;
 	}
 	void wait() {
 		std::unique_lock<std::mutex> lock(mtx);
-		cv.wait(lock, [this] {return !func_list.size(); });
-		is_running = false;
-	}
-	void join() {
-		if (!joined) {
-			joined = true;
-			is_running = true;
-			if (!func_list.size())
-				post([] {});
-			thread.join();
-			is_running = false;
-		}
+		cv.wait(lock, [this] { return !tasks.size(); });
 	}
 	size_t task_count() {
 		std::lock_guard<std::mutex> lock(mtx);
-		return func_list.size();
+		return tasks.size();
 	}
 };
 
 class thread_group {
 private:
-	std::condition_variable cv;
-	std::mutex mtx;
-	std::list<std::function<void()>> func_list;
-	const size_t thread_num;
-	std::unique_ptr<std::thread[]> threads;
-	std::atomic_bool joined = false;
-	std::atomic_bool is_running = false;
+	std::unique_ptr<thread_pool[]> thread_pools;
+	size_t thread_count;
 public:
-	thread_group(const size_t thread_num):
-	thread_num(thread_num)
-	{
-		threads = std::make_unique<std::thread[]>(thread_num);
-		for (size_t i = 0; i < thread_num; i++) {
-			threads[i] = std::thread([this] {
-				while (!joined || func_list.size() > this->thread_num) {
-					std::function<void()> func;
-					{
-						std::unique_lock<std::mutex> lock(mtx);
-						if (func_list.size() == 0) {
-							cv.wait(lock, [&] {return func_list.size() && is_running; });
-						}
-						func = std::move(func_list.front());
-						func_list.pop_front();
-					}
-					func();
-					cv.notify_all();
-				}
-			});
-		}
+	thread_group(const size_t thread_num) {
+		thread_pools = std::make_unique<thread_pool[]>(thread_num);
+		thread_count = thread_num;
 	}
-	~thread_group() {
-		join();
-	}
-	void post(std::function<void()> func) {
-		std::lock_guard<std::mutex> lock(mtx);
-		func_list.push_back(std::move(func));
-		cv.notify_all();
-	}
-	void run() {
-		is_running = true;
+	void post(size_t th, std::function<void()> func) {
+		thread_pools[th].post(std::move(func));
 	}
 	void wait() {
-		std::unique_lock<std::mutex> lock(mtx);
-		cv.wait(lock, [this] {return !func_list.size(); });
-		is_running = false;
-	}
-	void join() {
-		if (!joined) {
-			joined = true;
-			is_running = true;
-			if (func_list.size() < thread_num) {
-				for (size_t i = 0; i < thread_num; i++) {
-					post([] {});
-				}
-			}
-			for (size_t i = 0; i < thread_num; i++) {
-				threads[i].join();
-			}
-			is_running = false;
-		}
+		for (size_t i = 0; i < thread_count; ++i)
+			thread_pools[i].wait();
 	}
 	size_t task_count() {
-		std::lock_guard<std::mutex> lock(mtx);
-		return func_list.size();
+		size_t sum = 0;
+		for (size_t i = 0; i < thread_count; ++i)
+			sum += thread_pools[i].task_count();
 	}
 };
 
 
-
-template<class T, unsigned int align_N=64>
-class align_array{
+template<typename T>
+class aligned_array {
 private:
-	int d_align;
-	int d_size;
+	size_t d_align;
+	size_t d_size;
 	T** data;
 	struct iterator {
-		int counter;
-		const int size;
-		T*const * const data;
-		iterator(const int arg_size, T* const* const arg_data) noexcept :
+		size_t counter;
+		const size_t size;
+		T* const* const data;
+		iterator(const size_t arg_size, T* const* const arg_data) noexcept :
 			counter(0),
 			size(arg_size),
 			data(arg_data)
 		{
 		}
-		bool operator!=(iterator& iter) const noexcept {
-			return counter != iter.size;
-		}
-		T& operator*() {
-			return *data[counter];
-		}
-		const T& operator*() const{
-			return *data[counter];
-		}
+		bool operator!=(iterator& iter) const noexcept { return counter != iter.size; }
+		T& operator*() { return *data[counter]; }
+		const T& operator*() const { return *data[counter]; }
 		iterator& operator++() noexcept {
-			counter++;
+			++counter;
 			return *this;
 		}
 	};
-	void init(const int size, const int align) {
+	void init(const size_t size, const size_t align) {
 		d_size = size;
 		d_align = align;
-		data = new T*[size];
-		for (int i = 0; i < d_size; i++) {
+		data = new T * [size];
+		for (size_t i = 0; i < d_size; ++i) {
 #ifdef _WIN32
 			data[i] = new(_aligned_malloc(sizeof(T), align)) T;
 #endif
@@ -191,7 +125,7 @@ private:
 	}
 	void destroy() {
 		if (data != nullptr) {
-			for (int i = 0; i < d_size; i++) {
+			for (int i = 0; i < d_size; ++i) {
 				data[i]->~T();
 #ifdef _WIN32
 				_aligned_free(data[i]);
@@ -205,53 +139,28 @@ private:
 		}
 	}
 public:
-	align_array(const int size, const int align=align_N){
+	aligned_array(const size_t size, const size_t align = 64) { init(size, align); }
+	aligned_array(const aligned_array& src) {
+		init(src.d_size, src.d_align);
+		for (int i = 0; i < d_size; ++i)
+			*data[i] = *src.data[i];
+	}
+	aligned_array(aligned_array&& src) noexcept { operator=(std::move(src)); }
+	void resize(const size_t size, const size_t align = 64) {
+		destroy();
 		init(size, align);
 	}
-	align_array(const align_array& copy) {
-		(*this) = copy;
-	}
-	align_array(align_array&& move) noexcept {
-		d_size = move.d_size;
-		data = move.data;
-		move.data = nullptr;
-	}
-	void resize(const int size) {
-		destroy();
-		init(size, d_align);
-	}
-	align_array<T>& operator=(const align_array& copy) {
-		init(copy.d_size, copy.d_align);
-		for (int i = 0; i < d_size; i++) {
-			*data[i] = *copy.data[i];
-		}
-		return *this;
-	}
-	align_array<T>& operator=(align_array&& move) noexcept {
+	aligned_array<T>& operator=(aligned_array&& move) noexcept {
 		d_size = move.d_size;
 		data = move.data;
 		move.data = nullptr;
 		return *this;
 	}
-	~align_array() {
-		destroy();
-	}
-	T& operator[](const int index) {
-		return *(data[index]);
-	}
-	const T& operator[](const int index) const {
-		return *(data[index]);
-	}
-	const int size() const noexcept {
-		return d_size;
-	}
-	const int align() const noexcept {
-		return d_align;
-	}
-	iterator begin() const noexcept {
-		return iterator(d_size, data);
-	}
-	iterator end() const noexcept {
-		return iterator(d_size, data);
-	}
+	~aligned_array() { destroy(); }
+	T& operator[](const size_t index) { return *(data[index]); }
+	const T& operator[](const size_t index) const { return *(data[index]); }
+	const size_t size() const noexcept { return d_size; }
+	const size_t align() const noexcept { return d_align; }
+	iterator begin() const noexcept { return iterator(d_size, data); }
+	iterator end() const noexcept { return iterator(d_size, data); }
 };
